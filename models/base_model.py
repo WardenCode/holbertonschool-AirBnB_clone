e98802ce050ddcd8d11102a557c5f980c01e9f63
Carlos Soria <soriac367@gmail.com>
2022-06-27 12:45:39 -0500

    Added tests and fixed style

--- models/base_model.py
+++ models/base_model.py
@@ -5,39 +5,40 @@
 from uuid import uuid4
 from datetime import datetime
 
+
 class BaseModel():
-	"""
-	Class BaseModel to State, City, etc
-	"""
+    """
+        Class BaseModel to State, City, etc
+    """
 
-	def __init__(self):
-		"""
-		Constructor of Base Model
-		"""
-		self.id = str(uuid4())
-		self.created_at = datetime.now()
-		self.update_at = datetime.now()
+    def __init__(self):
+        """
+        Constructor of Base Model
+        """
+        self.id = str(uuid4())
+        self.created_at = datetime.now()
+        self.update_at = datetime.now()
 
-	def __str__(self):
-		"""
-		No official representation of Base Model
-		"""
-		return (f"[{self.__class__.__name__}] ({self.id}) <{self.__dict__}>")
+    def __str__(self):
+        """
+        Unofficial representation of Base Model
+        """
+        return (f"[{self.__class__.__name__}] ({self.id}) <{self.__dict__}>")
 
-	def save(self):
-		"""
-		Save the new changes with the actual time
-		"""
-		self.update_at = datetime.now()
+    def save(self):
+        """
+        Save the new changes with the actual time
+        """
+        self.update_at = datetime.now()
 
-	def to_dict(self):
-		"""
-		Representation in a dictionary of the instance
-		"""
-		model = self.__dict__
+    def to_dict(self):
+        """
+        Representation in a dictionary of the instance
+        """
+        model = self.__dict__
 
-		model["__class__"] = self.__class__.__name__
-		model["created_at"] = model["created_at"].isoformat()
-		model["update_at"] = model["update_at"].isoformat()
+        model["__class__"] = self.__class__.__name__
+        model["created_at"] = model["created_at"].isoformat()
+        model["update_at"] = model["update_at"].isoformat()
 
-		return (model)
+        return (model)
--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -1,13 +1,55 @@
 #!/usr/bin/python3
+"""This model defines several tests cases for the BaseModel class"""
+from distutils.ccompiler import new_compiler
 import unittest
 from models.base_model import BaseModel
-from datetime import datetime
+from datetime import datetime, date
+
 
 class Test_Base_Model(unittest.TestCase):
-	"""
-	
-	"""
+    """
+        Tests for BaseModel class
+    """
 
-	def test_init(self):
-		""""""
-		new_model = BaseModel()
+    def test_created_at(self):
+        """Tests 'created_at' time"""
+        new_model = BaseModel()
+        now = datetime.now().microsecond
+        then = new_model.created_at.microsecond
+        self.assertLess(now - then, 10)
+
+    def test_update(self):
+        """Tests 'update_at' time"""
+        new_model = BaseModel()
+        then = new_model.created_at.microsecond
+        new_model.save()
+        now = new_model.update_at.microsecond
+        self.assertGreater(now, then)
+
+    def test_id(self):
+        """Tests 'id' values """
+        new_model = BaseModel()
+        new_model2 = BaseModel()
+        self.assertNotEqual(new_model.id, new_model2.id)
+
+    def test_str(self):
+        """"Tests __str__ magic method"""
+        new_model = BaseModel()
+        str_repr = str(new_model)
+        self.assertIn(new_model.__class__.__name__, str_repr)
+        self.assertIn(new_model.id, str_repr)
+        self.assertIn(str(new_model.__dict__), str_repr)
+
+    def test_dict(self):
+        """Tests to 'to_dict' method"""
+        new_model = BaseModel()
+        to_dict = new_model.to_dict()
+        for i in to_dict:
+            self.assertIsInstance(i, str)
+        raised = False
+        try:
+            datetime.fromisoformat(to_dict["created_at"])
+            datetime.fromisoformat(to_dict["update_at"])
+        except ValueError as e:
+            raised = True
+        self.assertFalse(raised)
