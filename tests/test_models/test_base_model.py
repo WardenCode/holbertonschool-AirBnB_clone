17953d52fbf7b6fbf010c9438651f8ef393d02a8
WardenCode <diegojeanluck@hotmail.com>
2022-06-27 11:35:14 -0500

    Using more especific name of directories

---
+++ tests/test_models/test_base_model.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+import unittest
+from models.base_model import BaseModel
+from datetime import datetime
+
+class Test_Base_Model(unittest.TestCase):
+	"""
+	
+	"""
+
+	def test_init(self):
+		""""""
+		new_model = BaseModel()
