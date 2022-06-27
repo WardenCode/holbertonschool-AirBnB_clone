5456626084b291bf743cfa3bed8a5186d04d6641
Carlos Soria <soriac367@gmail.com>
2022-06-27 18:22:11 -0500

    First version of the shell

--- console.py
+++ console.py
@@ -1,1 +1,26 @@
 #!/usr/bin/python3
+""""In this module the class HBNBCommand is defined"""
+
+import cmd
+
+
+class HBNBCommand(cmd.Cmd):
+    """Class to make a python shell"""
+    prompt = "(HBTN)"
+
+    def do_quit(self, arg):
+        """Usage: quit,
+            quits the shell
+        """
+        exit()
+
+    def do_EOF(self, arg):
+        """Usage: EOF or CTRL+D
+            quits the shell
+        """
+        print()
+        exit()
+
+
+if __name__ == '__main__':
+    HBNBCommand().cmdloop()
