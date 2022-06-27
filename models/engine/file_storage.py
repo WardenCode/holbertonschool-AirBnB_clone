b3c4518f4d32921240587fc68da6c17eda3d85eb
WardenCode <diegojeanluck@hotmail.com>
2022-06-27 15:45:09 -0500

    Structure of class file storage

---
+++ models/engine/file_storage.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""
+
+"""
+import json
+
+
+class FileStorage():
+	""""""
+
+	__file_path = ".json"
+	__objects = {}
+
+	def all(self):
+		""""""
+		return (FileStorage.__objects)
+
+	def new(self, obj):
+		""""""
+
+
+	def save(self):
+		""""""
+		with open(self.__file_path, '', encoding='utf8') as file:
+
+
+	def reload(self):
+		""""""
+
+
+	def all(self):
+		""""""
