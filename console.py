b939f7e4227e5d0bd396d8440166a9751a9103f5
WardenCode <diegojeanluck@hotmail.com>
2022-06-28 08:54:11 -0500

    Fix return missing update & refactor in console functions

--- console.py
+++ console.py
@@ -9,6 +9,40 @@ class HBNBCommand(cmd.Cmd):
     """Class to make a python shell"""
     prompt = "(hbnb) "
 
+    @staticmethod
+    def validate_params(params, case):
+        """"""
+        if (case >= 0):
+            if not (params):
+                print("** class name missing **")
+                return (1)
+
+            if (params[0] not in ["BaseModel"]):
+                print("** class doesn't exist **")
+                return (1)
+
+        if (case >= 1):
+            len_params = len(params)
+
+            if (len_params == 1):
+                print("** instance id missing **")
+                return (1)
+
+            if not (storage.all().get(f"{params[0]}.{params[1]}", None)):
+                print("** no instance found **")
+                return (1)
+
+        if (case == 2):
+            if (len_params == 2):
+                print("** attribute name missing **")
+                return (1)
+
+            if (len_params == 3):
+                print("** value missing **")
+                return (1)
+
+        return (0)
+
     def do_quit(self, arg):
         """Usage: quit,\nquits the shell"""
         exit()
@@ -22,13 +56,7 @@ class HBNBCommand(cmd.Cmd):
         """"""
         params = arg.split()
 
-        if not (params):
-            print("** class name missing **")
-            return
-
-        if (params[0] not in ["BaseModel"]):
-            print("** class doesn't exist **")
-        else:
+        if not (HBNBCommand.validate_params(params, 0)):
             new_model = BaseModel()
             new_model.save()
             print(new_model.id)
@@ -37,52 +65,19 @@ class HBNBCommand(cmd.Cmd):
     def do_show(self, arg):
         """"""
         params = arg.split()
-        if not (params):
-            print("** class name missing **")
-            return
 
-        if (params[0] not in ["BaseModel"]):
-            print("** class doesn't exist **")
-            return
-
-        len_params = len(params)
-
-        if (len_params == 1):
-            print("** instance id missing **")
-            return
-
-        if not (storage.all().get(f"{params[0]}.{params[1]}", None)):
-            print("** no instance found **")
-            return
-
-        print(storage.all().get(f"BaseModel.{params[1]}"))
+        if not (HBNBCommand.validate_params(params, 1)):
+            print(storage.all().get(f"BaseModel.{params[1]}"))
 
 
     def do_destroy(self, arg):
         """"""
         params = arg.split()
 
-        if not (params):
-            print("** class name missing **")
-            return
-
-        if (params[0] not in ["BaseModel"]):
-            print("** class doesn't exist **")
-            return
-
-        len_params = len(params)
-
-        if (len_params == 1):
-            print("** instance id missing **")
-            return
-
-        if not (storage.all().get(f"{params[0]}.{params[1]}", None)):
-            print("** no instance found **")
-            return
-
-        element = storage.all()
-        del element[f"{params[0]}.{params[1]}"]
-        storage.save()
+        if not (HBNBCommand.validate_params(params, 1)):
+            element = storage.all()
+            del element[f"{params[0]}.{params[1]}"]
+            storage.save()
 
 
     def do_all(self, arg):
@@ -96,60 +91,30 @@ class HBNBCommand(cmd.Cmd):
         if (params[0] not in ["BaseModel"]):
             print("** class doesn't exist **")
         else:
-            print(storage.all())
+            print(storage.all()) # add filter
 
     def do_update(self, arg):
         """Usage: update <class name> <id> <attribute name> "<attribute value>"""
         params = arg.split()
 
-        if not (params):
-            print("** class name missing **")
-            return
+        if not (HBNBCommand.validate_params(params, 1)):
+            key = params[2]
+            value = params[3]
 
-        class_name = params[0]
+            if (key in ["id", "created_at", "updated_at"]):
+                print("Forbbiden")
+                return
 
-        if (class_name not in ["BaseModel"]):
-            print("** class doesn't exist **")
-            return
+            if (value.isdigit()):
+                value = int(value)
+            else:
+                try:
+                    value = float(value)
+                except ValueError:
+                    pass
 
-        len_params = len(params)
-
-        if (len_params == 1):
-            print("** instance id missing **")
-            return
-
-        id = params[1]
-
-        obj = storage.all().get(f"{class_name}.{id}", None)
-
-        if not (obj):
-            print("** no instance found **")
-            return
-
-        if (len_params == 2):
-            print("** attribute name missing **")
-            return
-
-        if (len_params == 3):
-            print("** value missing **")
-            return
-
-        key = params[2]
-        value = params[3]
-
-        if (key in ["id", "created_at", "updated_at"]):
-            print("Forbbiden")
-
-        if (value.isdigit()):
-            value = int(value)
-        else:
-            try:
-                value = float(value)
-            except ValueError:
-                pass
-
-        storage.all()[f"{class_name}.{id}"][key] = value
-        storage.save()
+            storage.all()[f"{params[0]}.{params[1]}"][key] = value
+            storage.save()
 
 
 if __name__ == '__main__':
